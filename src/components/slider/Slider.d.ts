fc795bfc395f3b148b689d2ec57b22056e3064fc
John Lombardo <john@programbo.com>
2018-04-21 19:19:41 +0300

    Add semicolons

--- src/components/slider/Slider.d.ts
+++ src/components/slider/Slider.d.ts
@@ -1,20 +1,20 @@
 import React = require("react");
 
-type Orientation = "horizontal" | "vertical"
-type SliderValue = number | [number, number]
+type Orientation = "horizontal" | "vertical";
+type SliderValue = number | number[];
 
 interface SliderProps {
-    id?: string
-    value?: SliderValue
-    animate?: boolean
-    min?: number
-    max?: number
-    orientation?: Orientation
-    step?: number
-    range?: boolean
-    style?: React.CSSProperties
-    className?: string
-    onChange: ({ originalEvent: Event, value: SliderValue }) => void
+    id?: string;
+    value?: SliderValue;
+    animate?: boolean;
+    min?: number;
+    max?: number;
+    orientation?: Orientation;
+    step?: number;
+    range?: boolean;
+    style?: React.CSSProperties;
+    className?: string;
+    onChange: ({ originalEvent: Event, value: SliderValue }) => void;
 }
 
 export class Slider extends React.Component<SliderProps, any> {}
