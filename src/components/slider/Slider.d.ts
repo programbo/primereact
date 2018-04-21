6ed4d370335621104ac0b382cc623d5866757ab7
John Lombardo <john@programbo.com>
2018-04-21 19:12:58 +0300

    Add onChange prop to slider
    Also adds a type hint for `orientation?`.

--- src/components/slider/Slider.d.ts
+++ src/components/slider/Slider.d.ts
@@ -1,16 +1,20 @@
 import React = require("react");
 
+type Orientation = "horizontal" | "vertical"
+type SliderValue = number | [number, number]
+
 interface SliderProps {
-    id?: string;
-    value?: number;
-    animate?: boolean;
-    min?: number;
-    max?: number;
-    orientation?: string;
-    step?: number;
-    range?: boolean;
-    style?: object;
-    className?: string;
+    id?: string
+    value?: SliderValue
+    animate?: boolean
+    min?: number
+    max?: number
+    orientation?: Orientation
+    step?: number
+    range?: boolean
+    style?: React.CSSProperties
+    className?: string
+    onChange: ({ originalEvent: Event, value: SliderValue }) => void
 }
 
-export class Slider extends React.Component<SliderProps,any> {}
+export class Slider extends React.Component<SliderProps, any> {}
